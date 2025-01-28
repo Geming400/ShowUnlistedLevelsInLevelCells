420315927e14809e37e4a302db7a5cb379b95142
Geming400 <Geming400@gmail.com>
2025-01-28 18:40:38 +0100

    Removed debugging things

--- src/levelClass.cpp
+++ src/levelClass.cpp
@@ -5,7 +5,5 @@
 using namespace geode::prelude;
 
 bool LevelClass::operator==(const LevelClass& givenClass) const {
-    log::debug("LevelClass::operator==() -> this.level: {}", level->m_levelName);
-    log::debug("LevelClass::operator==() -> givenClass.level: {}", givenClass.level->m_levelName);
     return level == givenClass.level;
 }
