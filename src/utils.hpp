d5215a2477d5766560a999802ec632f888b31972
Geming400 <Geming400@gmail.com>
2025-02-21 19:21:28 +0100

    Added utils

---
+++ src/utils.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+namespace Fades {
+    class Fades {
+    public:
+        static inline float clockFadeOutTime = 0.5f;
+        static inline float iconsFadeInTime = 0.8f;
+    };
+
+    class FadeTo {
+    public:
+        static inline unsigned int iconsFadeTo = 128;
+    };
+}
+
+namespace Ids {
+    const std::string CLOCK_SPRITE_ID = "clock-sprite"_spr;
+    const std::string UNLISTED_SPRITE_ID = "unlisted-icon.png"_spr;
+    const std::string FRIEND_ONLY_SPRITE_ID = "friend-only-icon.png"_spr
+}
